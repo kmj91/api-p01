44e74f777e667619d7a5e31a125ab7c38480a2da
kmj91 <lethita0302@gmail.com>
2022-01-05 20:58:41 +0900

    이미지 매니저
    주석 추가

--- Gunbird2/Gunbird_Game/BmpMgr.cpp
+++ Gunbird2/Gunbird_Game/BmpMgr.cpp
@@ -1,3 +1,6 @@
+// 기명준
+// bmp 이미지 리소스 매니저
+
 #include "stdafx.h"
 #include "BmpMgr.h"
 #include "MyBitmap.h"
@@ -13,25 +16,34 @@ CBmpMgr::~CBmpMgr()
 	Release();
 }
 
+// 이미지 파일을 불러와 HDC로 저장
+// _pFileName : 파일 주소
+// _pImagekey : map의 키 값
 void CBmpMgr::Insert_Bmp(const TCHAR* _pFileName, const TCHAR* _pImagekey)
 {
+	// 키 값을 사용하는지 확인, 없으면 end를 반환
 	auto iter = find_if(m_mapBit.begin(), m_mapBit.end(), CStrCmp(_pImagekey));
-
+	// 없으면 해당 키 값으로 생성
 	if (m_mapBit.end() == iter)
 	{
 		CMyBitmap* pBmp = new CMyBitmap;
+		// 이미지 파일 불러오기
 		pBmp->Load_Bmp(_pFileName);
-
 		m_mapBit.emplace(_pImagekey, pBmp);
 	}
 }
 
+// 이미지 HDC 반환
+// _pImagekey : 이미지 키 값
+// 반환 값 : 이미지 HDC
 HDC CBmpMgr::Find_Image(const TCHAR* _pImagekey)
 {
+	// 키 값을 사용하는 지 확인, 없으면 end를 반환
 	auto iter = find_if(m_mapBit.begin(), m_mapBit.end(), CStrCmp(_pImagekey));
-
+	// 해당 키 값을 사용하는 이미지가 없음
 	if (m_mapBit.end() == iter)
 		return nullptr;
+	// HDC 반환
 	return iter->second->Get_MemDC();
 }
 
--- Gunbird2/Gunbird_Game/BmpMgr.h
+++ Gunbird2/Gunbird_Game/BmpMgr.h
@@ -1,3 +1,6 @@
+// 기명준
+// bmp 이미지 리소스 매니저
+
 #pragma once
 
 #ifndef __BMPMGR_H__
@@ -11,7 +14,13 @@ private:
 	~CBmpMgr();
 
 public:
+	// 이미지 파일을 불러와 HDC로 저장
+	// _pFileName : 파일 주소
+	// _pImagekey : map의 키 값
 	void Insert_Bmp(const TCHAR* _pFileName, const TCHAR* _pImagekey);
+	// 이미지 HDC 반환
+	// _pImagekey : 이미지 키 값
+	// 반환 값 : 이미지 HDC
 	HDC Find_Image(const TCHAR* _pImagekey);
 	void Release();
 
