d4ee935bc3c71645e2b38af57c4246b8dc7ce891
kmj91 <lethita0302@gmail.com>
2022-01-09 19:47:14 +0900

    몬스터 캐논
    주석 추가

--- Gunbird2/Gunbird_Game/Cannon.cpp
+++ Gunbird2/Gunbird_Game/Cannon.cpp
@@ -1,3 +1,8 @@
+// 기명준
+// 숨은 캐논
+// 배치 전까지 무적 배치 후 무적 해제
+// 파괴되면 점수 아이템 생성과 스프라이트를 파괴된 잔해로 변경
+
 #include "stdafx.h"
 #include "Cannon.h"
 
@@ -22,32 +27,34 @@ CCannon::~CCannon()
 
 void CCannon::Initialize()
 {
+	// 기본 위치 및 설정
 	m_tInfo.iCX = CANNON_WIDTH * 3;
 	m_tInfo.iCY = CANNON_HEIGHT * 3;
 	m_tHitRectPos = { 6 * 3, 1 * 3, 26 * 3, 33 * 3 };
 	m_iImageWidth = CANNON_WIDTH;
 	m_iImageHeight = CANNON_HEIGHT;
-
+	// 체력
 	m_fHp = 60.f;
-
+	// 이미지 번호
 	m_iDrawID = CCannon::A_90;
+	// 기본 상태
 	m_eCurState = CCannon::HIDE;
-
+	// 공격 간격
 	m_dwShotDelay = 2000;
 
 	// 점수
 	m_iScore = 500;
 }
 
+// 업데이트
 int CCannon::Update()
 {
+	// 삭제
 	if (m_bRemove)
 		return OBJ_DEAD;
-
-	if (m_bDead)
-	{
-		if (m_ePreState != CCannon::DESTROY)
-		{
+	// 죽음
+	if (m_bDead) {
+		if (m_ePreState != CCannon::DESTROY) {
 			m_eCurState = CCannon::DESTROY;
 			m_bHpLock = true;		// 파괴되서 HP 다시 락
 			CObj* pObj = CAbstractFactory<CExplosion_02_1>::Create(m_tInfo.fX, m_tInfo.fY);		// 파괴 이펙트 생성
@@ -62,33 +69,28 @@ int CCannon::Update()
 		}
 	}
 
-	m_tInfo.fY += g_fBackgroundSpeed * 3.f;			// 배경에 맞춰서 Y축 이동
+	// 배경에 맞춰서 Y축 이동
+	m_tInfo.fY += g_fBackgroundSpeed * 3.f;
 
-	if (m_ePreState == CCannon::ON)
-	{
-		Aim();			// 조준
-		if (m_dwShotTime + m_dwShotDelay < GetTickCount())
-		{
+	// 배치 후
+	if (m_ePreState == CCannon::ON) {
+		// 조준
+		Aim();
+		if (m_dwShotTime + m_dwShotDelay < GetTickCount()) {
+			// 공격
 			Shot();
 			m_dwShotTime = GetTickCount();
 		}
 	}
-	else if(m_ePreState == CCannon::HIDE)
-	{
-		if (m_bOnObject)
-		{
-			if (m_tFrame.iFrameCnt == m_tFrame.iFrameEnd)
-			{
-				m_bHpLock = false;
-				m_eCurState = CCannon::ON;
-				m_dwShotTime = GetTickCount();
-			}	
-		}
-		else
-		{
-			if (m_tInfo.fY > m_fDestPosY)
-			{
-				m_bOnObject = true;						// 숨어있다가 나옴
+	// 배치 되기 전
+	else if(m_ePreState == CCannon::HIDE) {
+		// 아직 숨은 상태
+		if (!m_bOnObject) {
+			// 목표 Y 좌표를 넘어가면
+			if (m_tInfo.fY > m_fDestPosY) {
+				// 숨어있다가 나옴
+				m_bOnObject = true;
+				// 숨어있다가 나오는 애니메이션 프레임 재생
 				m_tFrame.iFrameCnt = 0;
 				m_tFrame.iFrameStart = 0;
 				m_tFrame.iFrameEnd = 6;
@@ -97,21 +99,38 @@ int CCannon::Update()
 				m_tFrame.dwFrameSpeed = 80;
 			}
 		}
+		// 숨어있다가 나옴
+		else {
+			// 애니메이션 프레임이 끝에 도달하면
+			if (m_tFrame.iFrameCnt == m_tFrame.iFrameEnd) {
+				// 무적 해제
+				m_bHpLock = false;
+				// 상태 변경
+				m_eCurState = CCannon::ON;
+				m_dwShotTime = GetTickCount();
+			}
+		}
 	}
 
+	// 이미지 RECT 정보 및 Hit RECT 정보 갱신
 	Update_Rect();
+	// 프레임 씬 변경 처리
 	Scene_Change();
+	// 애니메이션 프레임 이동
 	Frame_Move();
 
 	return OBJ_NOEVENT;
 }
 
+// 레이트 업데이트
 void CCannon::Late_Update()
 {
+	// 맵 하단 밖으로 나가면 삭제
 	if (WINCY <= m_tRect.top)
 		m_bRemove = true;
 }
 
+// 렌더
 void CCannon::Render(HDC _DC)
 {
 	HDC hMemDC;
@@ -151,7 +170,8 @@ void CCannon::Render(HDC _DC)
 			, RGB(255, 0, 255));
 	}
 
-	// 충돌 박스
+	// 만약 옵션에서 충돌 박스 보기를 켰다면 (넘버패드 1번 키)
+	// 충돌 박스도 렌더 해줘야함
 	if (!g_bHitRectRender) {
 		return;
 	}
@@ -162,12 +182,14 @@ void CCannon::Release()
 {
 }
 
+// 프레임 씬 변경 처리
 void CCannon::Scene_Change()
 {
 	if (m_ePreState != m_eCurState)
 	{
 		switch (m_eCurState)
 		{
+			// 숨어있음
 		case CCannon::HIDE:
 			m_tFrame.iFrameCnt = 0;
 			m_tFrame.iFrameStart = 0;
@@ -176,8 +198,10 @@ void CCannon::Scene_Change()
 			m_tFrame.dwFrameTime = GetTickCount();
 			m_tFrame.dwFrameSpeed = 999999;
 			break;
+			// 여기서 제어하지 않음 Aim()에서 제어함
 		case CCannon::ON:
 			break;
+			// 파괴
 		case CCannon::DESTROY:
 			m_tFrame.iFrameCnt = 0;
 			m_tFrame.iFrameStart = 0;
@@ -191,18 +215,21 @@ void CCannon::Scene_Change()
 	}
 }
 
+// 조준
 void CCannon::Aim()
 {
-	float	fX = m_pTarget->Get_Info().fX - m_tInfo.fX;
-	float	fY = m_pTarget->Get_Info().fY - m_tInfo.fY;
-	float	fDia = sqrtf(fX * fX + fY * fY);
-	float	fRad = acosf(fX / fDia);
-
+	float fX = m_pTarget->Get_Info().fX - m_tInfo.fX;	// 방향 좌표 X
+	float fY = m_pTarget->Get_Info().fY - m_tInfo.fY;	// 방향 좌표 Y
+	float fDia = sqrtf(fX * fX + fY * fY);				// 현재 위치에서 목적지까지 거리
+	// arccos(cos) = 라디안 값을 구함
+	float fRad = acosf(fX / fDia);
+	// 라디안 값으로 호도각을 구함
 	m_fAngle = fRad * 180.f / PI;
-
+	// 타겟의 Y 좌표가 나의 Y 좌표 보다 크면 부호 반전
 	if (m_tInfo.fY < m_pTarget->Get_Info().fY)
 		m_fAngle *= -1.f;
 
+	// 위에서 구한 호도각으로 스프라이트 인덱스 번호 변경
 	if (-5.f <= m_fAngle && m_fAngle < 5.f)
 		m_iDrawID = CCannon::A0;
 	else if (-10.f <= m_fAngle && m_fAngle < -5.f)
@@ -269,8 +296,10 @@ void CCannon::Aim()
 		m_iDrawID = CCannon::A5;
 }
 
+// 공격
 void CCannon::Shot()
 {
+	// 총알 오브젝트 생성
 	CObj* pObj = CAbstractFactory<CBullet_2>::Create(m_tInfo.fX, m_tInfo.fY);
 	pObj->Set_Angle(m_fAngle);
 	CObjMgr::Get_Instance()->Add_Object(pObj, OBJID::MONSTER_BULLET);
