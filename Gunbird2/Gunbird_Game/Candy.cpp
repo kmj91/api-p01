13cda7e6d12e527fe52e482936a9eea9e1b4b2db
kmj91 <lethita0302@gmail.com>
2022-01-14 10:29:51 +0900

    사탕 아이템
    주석 추가

--- Gunbird2/Gunbird_Game/Candy.cpp
+++ Gunbird2/Gunbird_Game/Candy.cpp
@@ -1,3 +1,8 @@
+// 기명준
+// 폭탄 사용시 적 총알이 변환된 사탕 오브젝트
+// 순간적으로 떠오르다가 맵 하단으로 쭉 떨어짐
+// 충돌하면 점수 획득
+
 #include "stdafx.h"
 #include "Candy.h"
 
@@ -20,23 +25,29 @@ CCandy::~CCandy()
 
 void CCandy::Initialize()
 {
+	// 이미지 크기 및 Hit RECT 초기화
 	m_tInfo.iCX = CANDY_WIDTH * 3;
 	m_tInfo.iCY = CANDY_HEIGHT * 3;
 	m_tHitRectPos = { 0, 0, CANDY_WIDTH * 3, CANDY_HEIGHT * 3 };
 	m_iImageWidth = CANDY_WIDTH;
 	m_iImageHeight = CANDY_HEIGHT;
-
+	// 여러 종류의 사탕 랜덤 선택
 	m_tFrame.iFrameScene = rand() % 3;
 	m_iDrawID = rand() % 22;
+	// 랜덤 각도
 	m_fAngle = (float)(90 + (rand() % 20 - 10));
+	// 이동 속도
 	m_fSpeed = 3.f;
-
+	// 아이템 타입 초기화
 	m_enItemType = ITEM::SCORE;
+	// 점수
 	m_iScore = 200;
 }
 
+// 업데이트
 int CCandy::Update()
 {
+	// 삭제
 	if (m_bRemove)
 		return OBJ_DEAD;
 
@@ -53,24 +64,33 @@ int CCandy::Update()
 		return OBJ_DEAD;
 	}
 
+	// 10보다 작으면 지속적으로 증가
 	if (m_fForce < 10.f)
 		m_fForce += 0.05f;
 
+	// 이동
+	// 속도 값이 +부호에서 -로 변하면서 점점 빠르게 맵 하단으로 떨어짐
 	m_tInfo.fX += cosf(m_fAngle * PI / 180.f) * (m_fSpeed - m_fForce);
 	m_tInfo.fY -= sinf(m_fAngle * PI / 180.f) * (m_fSpeed - m_fForce);
 
 	return OBJ_NOEVENT;
 }
 
+// 레이트 업데이트
 void CCandy::Late_Update()
 {
+	// 이 오브젝트가 Update에서 생성되서 Update를 건너 뛰고 Late_Update를 하는 경우가 발생함
+	// 그래서 Late_Udpate에서 갱신함
+	// 이미지 RECT 정보 및 Hit RECT 정보 갱신
 	Update_Rect();
 
+	// 맵 바깥으로 나가면 삭제
 	if (0 >= m_tRect.right || 0 >= m_tRect.bottom
 		|| WINCX <= m_tRect.left || WINCY <= m_tRect.top)
 		m_bRemove = true;
 }
 
+// 렌더
 void CCandy::Render(HDC _DC)
 {
 	HDC hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"Candy");
--- Gunbird2/Gunbird_Game/Candy.h
+++ Gunbird2/Gunbird_Game/Candy.h
@@ -1,3 +1,8 @@
+// 기명준
+// 폭탄 사용시 적 총알이 변환된 사탕 오브젝트
+// 순간적으로 떠오르다가 맵 하단으로 쭉 떨어짐
+// 충돌하면 점수 획득
+
 #pragma once
 
 #ifndef __CANDY_H__
@@ -19,7 +24,7 @@ public:
 	virtual void Release() override;
 
 private:
-	float		m_fForce;
+	float		m_fForce;	// 가중치
 };
 
 #endif // !__CANDY_H__
