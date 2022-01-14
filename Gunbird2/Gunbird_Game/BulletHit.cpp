2c1d15f52f2413f7046f45390e5aa935b186c163
kmj91 <lethita0302@gmail.com>
2022-01-15 00:16:23 +0900

    총알 피격 이펙트
    주석 추가

--- Gunbird2/Gunbird_Game/BulletHit.cpp
+++ Gunbird2/Gunbird_Game/BulletHit.cpp
@@ -1,3 +1,7 @@
+// 기명준
+// 플레이어 총알이 명중하면 생성되는 이펙트
+// 좌우 반전된 두 종류의 이미지를 사용 함
+
 #include "stdafx.h"
 #include "BulletHit.h"
 
@@ -16,11 +20,12 @@ CBulletHit::~CBulletHit()
 
 void CBulletHit::Initialize()
 {
+	// 이미지 크기 초기화
 	m_tInfo.iCX = BULLET_HIT_WIDTH * 3;
 	m_tInfo.iCY = BULLET_HIT_HEIGHT * 3;
 	m_iImageWidth = BULLET_HIT_WIDTH;
 	m_iImageHeight = BULLET_HIT_HEIGHT;
-
+	// 이미지 프레임 초기화
 	m_tFrame.iFrameCnt = 0;
 	m_tFrame.iFrameStart = 0;
 	m_tFrame.iFrameEnd = 17;
@@ -29,24 +34,33 @@ void CBulletHit::Initialize()
 	m_tFrame.dwFrameSpeed = 10;
 }
 
+// 업데이트
 int CBulletHit::Update()
 {
+	// 삭제
 	if (m_bDead)
 		return OBJ_DEAD;
 
+	// 이미지 프레임 이동
 	Frame_Move();
 
-	if (m_tFrame.iFrameCnt == m_tFrame.iFrameEnd)		// 프레임이 끝이면 사라짐
+	// 프레임이 끝이면 삭제
+	if (m_tFrame.iFrameCnt == m_tFrame.iFrameEnd)
 		m_bDead = true;
 
 	return OBJ_NOEVENT;
 }
 
+// 레이트 업데이트
 void CBulletHit::Late_Update()
 {
+	// 이 오브젝트가 Update에서 생성되서 Update를 건너 뛰고 Late_Update를 하는 경우가 발생함
+	// 그래서 Late_Udpate에서 갱신함
+	// 이미지 RECT 정보 및 Hit RECT 정보 갱신
 	Update_Rect();
 }
 
+// 렌더
 void CBulletHit::Render(HDC _DC)
 {
 	HDC hMemDC = CBmpMgr::Get_Instance()->Find_Image(L"BulletHit");
